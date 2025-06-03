9250c11a59444fa41706f00d9ffb1729cfc77b8d
Suzzene Darekhan <116281847+Suzzene@users.noreply.github.com>
2025-06-03 15:26:29 +0530

    Update main.py

--- main.py
+++ main.py
@@ -66,7 +66,7 @@ elif(app_mode=="DISEASE RECOGNITION"):
         st.image(test_image,width=4,use_column_width=True)
     #Predict button
     if(st.button("Predict")):
-        st.snow()
+        #st.snow()
         st.write("Our Prediction")
         result_index = model_prediction(test_image)
         #Reading Labels
